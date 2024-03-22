80a46478ca572f4bf88d6b2eaf9560b4d3a3a35a
Leejunhyeok03 <ly0609@naver.com>
2024-03-22 16:41:23 +0900

    feat:Loding, error State add

--- src/basic/components/Products.jsx
+++ src/basic/components/Products.jsx
@@ -1,20 +1,30 @@
 import React, { useEffect, useState } from 'react';
 
 export default function Products() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
   const [checked, setChecked] = useState(false);
   const [products, setProducts] = useState([]);
   const handleChange = () => setChecked((prev) => !prev);
   useEffect(() => {
+    setLoading(true);
+    setError(undefined);
     fetch(`data/${checked ? 'sale_' : ''}products.json`)
       .then((res) => res.json())
       .then((data) => {
         console.log('🔥뜨끈한 데이터를 네트워크에서 받아옴');
         setProducts(data);
-      });
+        setLoading(false);
+
+      })
+      .catch((e) => setError('에러가 발생했음!'))
+      .finally(() => setLoading(false));
     return () => {
       console.log('🧹 깨끗하게 청소하는 일들을 합니다.');
     };
   }, [checked]);
+  if(loading) return <p>Loading...</p>
+  if(error) return <p>{error}</p>
 
   return (
     <>
@@ -33,3 +43,4 @@ export default function Products() {
     </>
   );
 }
+
